ce0dec91f9aa5d62c2337df6065c95d062084ef7
genelocated <genelocated@foxmail.com>
2018-08-15 04:42:51 +0800

    optimise

--- dot.py
+++ dot.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import os, sys
+from os.path import join, isfile, isdir
 
 def help_():
     print(
@@ -46,6 +47,15 @@ def list_():
         # list dotfiles under a topic
         print(_getfiles(_prefixtopic(sys.argv[2])))
 
+def _rmrootslash(dir_):
+    """It is impossible to join '/opt/someplace' and '/home/user/' together
+    using os.path.join. We must remove one of the root splash.
+    """
+    if dir_[0] == '/':
+        return dir_[1:]
+    else:
+        raise ValueError('do not contain root slash')
+
 def _prefixtopic(topic):
     """Prefix with 'topic.' if not given"""
     return topic if topic.find('topic.') != -1 \
@@ -54,17 +64,17 @@ def _prefixtopic(topic):
 def _gettopic():
     file_and_dir = os.listdir('.')
     dironly = [i for i in file_and_dir \
-        if os.path.isdir(os.path.join('.', i))]
-    topics = [i[6:] for i in dironly if i.find('topic.') == 0]
+        if isdir(join('.', i))]
+    topics = [i[len('topic.'):] for i in dironly if i.find('topic.') == 0]
     return topics
 
 def _getfiles(topic):
-    os.chdir(os.path.join('.', topic))
+    os.chdir(join('.', topic))
     dirs = os.walk('.')
     # os.walk returns a list of tuples,
     # every tuple is (root: str, dirs: list, files: list)
     # files = [f for i in dirs for f in i[2]]
-    files = [os.path.join(i[0], f)[1:] for i in dirs for f in i[2]]
+    files = [join(i[0], f)[1:] for i in dirs for f in i[2]]
     os.chdir('..')
     return files
 
@@ -75,21 +85,21 @@ def apply():
     else:
         topic = _prefixtopic(sys.argv[2])
         for target in _getfiles(topic):
-            ori = os.path.join(os.getcwd(), topic, target[1:])
+            ori = join(os.getcwd(), topic, _rmrootslash(target))
             print(ori)
             print(target)
             # make dir if not exist
-            dir = os.path.split(target)[0]
-            if not os.path.isdir(dir):
-                os.makedirs(dir)
+            dir_ = os.path.split(target)[0]
+            if not isdir(dir_):
+                os.makedirs(dir_)
             # backup the file if already exist
-            if os.path.isfile(target):
+            if isfile(target):
                 os.rename(target, target + '.BAK')
             os.system('ln -s {ori} {tar}'.format(ori=ori, tar=target))
 
 def select():
     """Select a topic to commit."""
-    if len(sys.argv) == 2 and os.path.isfile('SELECTEDTOPIC'):
+    if len(sys.argv) == 2 and isfile('SELECTEDTOPIC'):
         os.remove('SELECTEDTOPIC')
     elif len(sys.argv) == 3:
         with open('SELECTEDTOPIC', 'w') as f:
@@ -104,13 +114,13 @@ def add():
         from shutil import copy
         for f in sys.argv[2:]:
             # remove root slash
-            dir = os.path.join('BUFFER', f[1:])
-            if not os.path.isdir(dir):
-                os.makedirs(dir)
-            copy(sys.argv[2], os.path.join(dir))
+            dir_ = join('BUFFER', _rmrootslash(f))
+            if not isdir(dir_):
+                os.makedirs(dir_)
+            copy(sys.argv[2], join(dir_))
 
 def status():
-    if os.path.isfile('SELECTEDTOPIC'):
+    if isfile('SELECTEDTOPIC'):
         print('Selected topic:')
         print('  (use `{} select` to unselect)'.format(sys.argv[0]))
         with open('SELECTEDTOPIC', 'r') as f:
@@ -118,7 +128,7 @@ def status():
     else:
         print('No topic selected.')
     print()
-    if os.path.isdir('BUFFER') and len(_getfiles('BUFFER')) != 0:
+    if isdir('BUFFER') and len(_getfiles('BUFFER')) != 0:
             print('Files staged in the buffer:')
             print(_getfiles('BUFFER'))
     else:
@@ -136,10 +146,10 @@ def commit():
     """Commit the files in the buffer to the selected topic.
     Create one if not exist.
     """
-    if not (os.path.isdir('BUFFER') and len(_getfiles('BUFFER')) != 0):
+    if not (isdir('BUFFER') and len(_getfiles('BUFFER')) != 0):
         print('Fatal: no file staged in the buffer')
         print('  (use `{} add <file> ...` to stage files)'.format(sys.argv[0]))
-    elif not os.path.isfile('SELECTEDTOPIC'):
+    elif not isfile('SELECTEDTOPIC'):
         print('Fatal: no topic selected')
         print('  (use `{} select <topic>` to select a topic)'.format(sys.argv[0]))
     else:
