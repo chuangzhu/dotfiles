aff398a2197dd088b70af5668f0dbd9661a1c386
genelocated <genelocated@foxmail.com>
2018-08-14 20:33:54 +0800

    chdir

--- dot.py
+++ dot.py
@@ -5,10 +5,10 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import os, sys
 
-def help():
+def help_():
     print(
-'''DOT:
-    A naive dotfiles management util by geneLocated
+"""DOT:
+    Naive dotfiles management util by geneLocated
 
 usage:
     `make [option] topic=[topic]` or
@@ -18,17 +18,21 @@ options:
     help:		Display this message
     list:		List exist topics
     list [topic]:	List .files under this topic
-    apply [topic]:	Apply a topic'''.format(filename=sys.argv[0])
+    apply [topic]:	Apply a topic""".format(filename=sys.argv[0])
         )
 
-def ls():
+def list_():
     if len(sys.argv) < 3:
         # list topics in this repo
         print(_gettopic())
     else:
         # list dotfiles under a topic
-        print(_getfiles(sys.argv[2]))
-        
+        print(_getfiles(_prefixtopic(sys.argv[2])))
+
+def _prefixtopic(topic):
+    """prefix with 'topic.' if not given"""
+    return topic if topic.find('topic.') != -1 \
+        else 'topic.' + topic
 
 def _gettopic():
     file_and_dir = os.listdir('.')
@@ -38,19 +42,24 @@ def _gettopic():
     return topics
 
 def _getfiles(topic):
-    dirs = os.walk(os.path.join('.', topic))
+    os.chdir(os.path.join('.', topic))
+    dirs = os.walk('.')
     # os.walk returns a list of tuples,
     # every tuple is (root: str, dirs: list, files: list)
     # files = [f for i in dirs for f in i[2]]
-    files = [os.path.join(i[0], f) for i in dirs for f in i[2]]
+    files = [os.path.join(i[0], f)[1:] for i in dirs for f in i[2]]
+    os.chdir('..')
     return files
 
 def apply():
     if len(sys.argv) < 3:
-        print('Missing topics, operation failed.')
+        print('Missing topic, operation failed.')
     else:
-        for i in _getfiles(sys.argv[2]):
-            os.system('ln -s {}'.format(i))
+        topic = _prefixtopic(sys.argv[2])
+        for i in _getfiles(topic):
+            ori = os.path.join('.', topic, i)
+            print(ori)
+            # os.system('ln -s {ori} {tar}'.format(ori=ori, tar=i))
 
 
 
@@ -58,9 +67,9 @@ if __name__ == '__main__':
     # print(sys.argv)
     # display help if no args provided
     if len(sys.argv) < 2:
-        help()
+        help_()
     else:
-        {'help': help,
-         'list': ls,
-         'ls': ls,
+        {'help': help_,
+         'list': list_,
+         'ls': list_,
          'apply': apply}[sys.argv[1]]()
